7e303fd05a352e4f45ae46fc0edcc8f79a7e3ef4
qlimenoque <qlimenoque@macpaw.com>
2025-11-13 23:09:32 +0200

    revert: course-app changes

--- apps/course-app/src/main.py
+++ apps/course-app/src/main.py
@@ -6,7 +6,6 @@ import threading
 import sqlite3
 from datetime import datetime
 from typing import List, Dict, Any, Optional
-from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException, Form, Query, Request
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -291,23 +290,11 @@ async def add_request_id(request: Request, call_next):
     response.headers["X-Request-ID"] = req_id
     return response
 
-## @app.on_event("startup")
-## def on_startup():
-##     store.init()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Code to run on startup
-    print("Application startup!")
-    yield
-    # Code to run on shutdown
-    print("Application shutdown!")
+@app.on_event("startup")
+def on_startup():
+    store.init()
 
-app = FastAPI(lifespan=lifespan)
-
-## @app.get("/")
-## async def read_root():
-##    return {"message": "Hello World"}
 
 @app.get("/", response_class=HTMLResponse)
 def index():
